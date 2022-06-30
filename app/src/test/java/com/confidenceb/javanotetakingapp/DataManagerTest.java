16333cd6948071e5fa3c63e035159c4d95544345
Confidence-bassey <confidencebass2@gmail.com>
2022-06-30 16:34:26 +0100

    Added a once per class pre-processing method to our test class

--- app/src/test/java/com/confidenceb/javanotetakingapp/DataManagerTest.java
+++ app/src/test/java/com/confidenceb/javanotetakingapp/DataManagerTest.java
@@ -3,33 +3,38 @@ package com.confidenceb.javanotetakingapp;
 import junit.framework.TestCase;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataManagerTest {
 
+    static DataManager sDataManager;
+    @BeforeClass
+    public static void myClassSetUp(){
+        sDataManager = DataManager.getInstance();
+    }
+
     @Before
     public void setUpMethod(){
-        DataManager dm = DataManager.getInstance();
-        dm.getNotes().clear();
-        dm.initializeExampleNotes();
+        sDataManager.getNotes().clear();
+        sDataManager.initializeExampleNotes();
     }
 
     @Test
     public void testCreateNewNote() {
-        DataManager dm = DataManager.getInstance();
-        CourseInfo course = dm.getCourse("android_sync");
+        CourseInfo course = sDataManager.getCourse("android_sync");
         final String noteTitle = "Note title";
         final String noteText = "My note text";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
         assertEquals(compareNote.getCourse(), course);
         assertEquals(compareNote.getTitle(), noteTitle);
         assertEquals(compareNote.getText(), noteText);
@@ -37,28 +42,27 @@ public class DataManagerTest {
 
     @Test
     public void findSimilarNotes() {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText1 = "This is the body text of my test note";
         final String noteText2  = "This is the body of my second test note";
 
-        int noteIndex1 = dm.createNewNote();
-        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        int noteIndex1 = sDataManager.createNewNote();
+        NoteInfo newNote1 = sDataManager.getNotes().get(noteIndex1);
         newNote1.setCourse(course);
         newNote1.setTitle(noteTitle);
         newNote1.setText(noteText1);
 
-        int noteIndex2 = dm.createNewNote();
-        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        int noteIndex2 = sDataManager.createNewNote();
+        NoteInfo newNote2 = sDataManager.getNotes().get(noteIndex2);
         newNote2.setCourse(course);
         newNote2.setTitle(noteTitle);
         newNote2.setText(noteText2);
 
-        int foundIndex1 = dm.findNote(newNote1);
+        int foundIndex1 = sDataManager.findNote(newNote1);
         assertEquals(noteIndex1, foundIndex1);
 
-        int foundIndex2 = dm.findNote(newNote2);
+        int foundIndex2 = sDataManager.findNote(newNote2);
         assertEquals(noteIndex2, foundIndex2);
     }
 }
