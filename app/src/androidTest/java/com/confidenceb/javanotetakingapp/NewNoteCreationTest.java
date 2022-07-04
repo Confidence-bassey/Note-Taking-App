e104be9d53f2f060d2b33c7c98308c914993e37e
Confidence-bassey <confidencebass2@gmail.com>
2022-07-04 14:15:13 +0100

    Added code to implement new note creation test

--- app/src/androidTest/java/com/confidenceb/javanotetakingapp/NewNoteCreationTest.java
+++ app/src/androidTest/java/com/confidenceb/javanotetakingapp/NewNoteCreationTest.java
@@ -1,5 +1,6 @@
 package com.confidenceb.javanotetakingapp;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -8,6 +9,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 
 @RunWith(AndroidJUnit4.class)
 public class NewNoteCreationTest {
@@ -22,6 +27,11 @@ public class NewNoteCreationTest {
 
     @Test
     public void createNewNote(){
-
+        //ViewInteraction newNoteFab = onView(withId(R.id.fab));
+        //newNoteFab.perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.note_title)).perform(typeText("note title"));
+        onView(withId(R.id.note_text_body)).perform(typeText("note body"),
+        closeSoftKeyboard());
     }
 }
